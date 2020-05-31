4bd69f2dd8531c60dc57c6ea80e2b1cea520582c
Alexandr <theobroma222@gmail.com>
2020-05-31 21:30:05 +0300

    add Logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from 'react';
 import './assets/styles/index.scss';
+// import { Layout } from './components/Layout/Layout';
+// import { Sort } from './components/Sort/Sort';
+// import { Filters } from './components/Filters/Filters';
+// import { TicketsList } from './components/TicketsList/TicketsList';
 
-function App() {
+// import { Filter } from './core/types/Filter';
+// import { SortType } from './core/types/Sort.type';
+// import { plural } from './core/lib/plural';
+// import { useTickets } from './core/hooks/useTickets';
+
+import Logo from './components/Logo/Logo';
+
+interface Props {
+  // activeTodoCount: number;
+}
+
+const App: React.FC<Props> = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Logo />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from './Logo.svg';
+
+const Logo: React.FC = () => {
+  return <img src={logo} alt="Логотип Aviasales" />;
+};
+
+export default Logo;
