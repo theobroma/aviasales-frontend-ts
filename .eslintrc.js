2c1ce67edf5f9e5ff62a0201941d0ffd2640623d
Alexandr <theobroma222@gmail.com>
2020-05-31 20:19:14 +0300

    upd configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,12 @@
 // .eslintrc.js
 module.exports = {
-  extends: ['airbnb-typescript-prettier'],
+  extends: ['airbnb-typescript-prettier', 'plugin:jest/recommended'],
   rules: {
     'no-param-reassign': 0,
     '@typescript-eslint/no-use-before-define': 0,
     'no-console': 0,
+    'react/state-in-constructor': 0,
+    'react/jsx-props-no-spreading': 0,
   },
   env: {
     'jest/globals': true,
