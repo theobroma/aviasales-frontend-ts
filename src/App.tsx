ab8057636e690af69866764a44285e382aae2834
Alexandr <theobroma222@gmail.com>
2020-06-02 15:59:15 +0300

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React, { useCallback, useState } from 'react';
 import './assets/styles/index.scss';
 import Layout from './components/Layout/Layout';
-// import { Sort } from './components/Sort/Sort';
 import Filters from './components/Filters/Filters';
 import { TicketsList } from './components/TicketsList/TicketsList';
-// import { Ticket } from './core/types/Ticket';
 import { SortType } from './core/types/Sort.type';
-
 import { Filter } from './core/types/Filter';
-// import { SortType } from './core/types/Sort.type';
 import { plural } from './core/lib/plural';
 import { useTickets } from './core/hooks/useTickets';
 
@@ -17,7 +13,7 @@ import Sort from './components/Sort/Sort';
 // import TicketsData from './core/mock/TicketsMock';
 
 interface Props {
-  // activeTodoCount: number;
+  // none;
 }
 
 const App: React.FC<Props> = () => {
